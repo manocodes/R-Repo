78e384a1ee03441ca21fd4db057641765692bbdc
manocodes <mano.net@gmail.com>
2016-08-07 11:30:39 -0600

    committing all

--- strings.R
+++ strings.R
@@ -1,1 +1,12 @@
 #lets experiment with string operations..
+str1 = "string 01"
+str2 = "string 02"
+str3 = "string 03"
+
+print(paste(str1,str2,str3))
+print(paste(str1,str2,str3, sep="."))
+print(paste(str1,str2,str3, sep=".", collapse=""))
+ 
+print(nchar("Count the number of characters"))
+
+#R string functions are awsome...
