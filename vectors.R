8a33339d4d7576d6f7c98aab0776e21e68a1f139
manocodes <mano.net@gmail.com>
2016-08-07 11:53:08 -0600

    committing all

---
+++ vectors.R
@@ -0,0 +1,18 @@
+#lets learn about vectors
+vec = c(10,20,30,40,50,60,70,80,90,100)
+print(vec)
+
+half = vec[3]
+print(half)
+
+half = vec[c(3,2)]
+print(half)
+
+half = vec[c(-1,-10)]
+print(half)
+
+half = vec[c(0,0,0,2,3,2,0)]
+print(half)
+
+numberlist = c(10,34,64,3,7,00,0,0,32,6457)
+print(sort(numberlist))
